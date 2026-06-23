1645d58e5f0244ca797de625582957319c1db869
Adrien Leteinturier <aleteinturier@fdjunited.com>
2026-06-23 12:27:06 +0200

    streams tp

---
+++ 02-streams/tp/transforms/anonymizer.js
@@ -0,0 +1,31 @@
+import { Transform } from 'node:stream';
+
+const EMAIL = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}/gi;
+
+export class Anonymizer extends Transform {
+    constructor(options) {
+        super(options);
+        this.tail = '';
+    }
+
+    _transform(chunk, encoding, callback) {
+        const data = this.tail + chunk.toString('utf8');
+        const lines = data.split('\n');
+        this.tail = lines.pop();
+        for (const line of lines) {
+            this.push(this.clean(line) + '\n');
+        }
+        callback();
+    }
+
+    _flush(callback) {
+        if (this.tail.length > 0) {
+            this.push(this.clean(this.tail));
+        }
+        callback();
+    }
+
+    clean(line) {
+        return line.replace(EMAIL, '***@***').toUpperCase();
+    }
+}
